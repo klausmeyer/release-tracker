ea14066a74d756c8513808367aea744484186921
Klaus Meyer <spam@klaus-meyer.net>
2024-02-11 00:30:22 +0100

    released_at fallback via commit

--- app/jobs/versions/update_release_date_job.rb
+++ app/jobs/versions/update_release_date_job.rb
@@ -1,17 +1,46 @@
 module Versions
   class UpdateReleaseDateJob < ApplicationJob
     def perform(version)
-      release = client.release_for_tag(version.project.slug, version.git_tag)
-      date    = release[:published_at] || release[:created_at]
+      self.version = version
 
-      Rails.logger.debug "[#{self.class.name}] #{version.project.slug} :: #{version.git_tag} :: #{date}"
+      timestamp = try_with_release || try_with_commit
 
-      version.update! released_at: date
-    rescue Octokit::NotFound
-      Rails.logger.debug "[#{self.class.name}] #{version.project.slug} :: #{version.git_tag} :: Not found"
+      Rails.logger.debug "[#{self.class.name}] #{version.project.slug} :: #{version.git_tag} :: #{timestamp}"
+
+      version.update! released_at: timestamp
     end
 
     private
+
+    attr_accessor :version
+
+    def try_with_release
+      return if release.nil?
+
+      release[:published_at] || release[:created_at]
+    end
+
+    def try_with_commit
+      return if commit.nil?
+
+      commit.dig(:commit, :committer, :date) || commit.dig(:commit, :author, :date)
+    end
+
+    def release
+      @release ||= client.release_for_tag(version.project.slug, version.git_tag)
+    rescue Octokit::NotFound
+      Rails.logger.debug "[#{self.class.name}] #{version.project.slug} :: #{version.git_tag} :: Not found"
+
+      nil
+    end
+
+    def commit
+      @commit ||= client.commit(version.project.slug, version.git_sha)
+    rescue Octokit::NotFound
+      Rails.logger.debug "[#{self.class.name}] #{version.project.slug} :: #{version.git_sha} :: Not found"
+
+      nil
+    end
 
     def client
       @client ||= Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
