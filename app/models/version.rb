502e7eea2b577d2cc29ce0782acd35342073e5a0
Klaus Meyer <spam@klaus-meyer.net>
2024-02-24 21:39:11 +0100

    fix semantic sorting (#16)

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,14 @@
 class Version < ApplicationRecord
   belongs_to :project
 
-  scope :semantic, -> { order(major: :asc, minor: :asc, patch: :asc, pre: :asc) }
+  scope :semantic, -> {
+    order(
+      :major,
+      :minor,
+      arel_table.coalesce(arel_table[:patch], 0),
+      arel_table.coalesce(arel_table[:pre], 0)
+    )
+  }
   scope :stable, -> { all }
   scope :latest, -> { where.not(released_at: nil).order(released_at: :desc) }
 
