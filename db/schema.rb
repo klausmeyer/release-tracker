d7fe397001b698fc22d58b3bf8458b69a5af4571
Klaus Meyer <spam@klaus-meyer.net>
2026-04-13 15:20:42 +0200

    drop monkey patch for schema dumper (#283)

--- db/schema.rb
+++ db/schema.rb
@@ -15,35 +15,35 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_26_090745) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "projects", force: :cascade do |t|
-    t.string "source", null: false
-    t.string "slug", null: false
-    t.string "name", null: false
-    t.bigint "version_pattern_id"
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.string "slug", null: false
+    t.string "source", null: false
     t.datetime "updated_at", null: false
+    t.bigint "version_pattern_id"
     t.index ["source", "slug"], name: "index_projects_on_source_and_slug", unique: true
     t.index ["version_pattern_id"], name: "index_projects_on_version_pattern_id"
   end
 
   create_table "version_patterns", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "examples", null: false
-    t.string "regexp", null: false
     t.datetime "created_at", null: false
+    t.string "examples", null: false
+    t.string "name", null: false
+    t.string "regexp", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_version_patterns_on_name", unique: true
   end
 
   create_table "versions", force: :cascade do |t|
-    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.string "git_sha", null: false
+    t.string "git_tag", null: false
     t.integer "major", null: false
     t.integer "minor", null: false
     t.integer "patch"
     t.integer "pre"
-    t.string "git_tag", null: false
-    t.string "git_sha", null: false
+    t.bigint "project_id", null: false
     t.datetime "released_at", precision: nil
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id", "major", "minor", "patch", "pre"], name: "idx_on_project_id_major_minor_patch_pre_94543b0152", unique: true
     t.index ["project_id"], name: "index_versions_on_project_id"
