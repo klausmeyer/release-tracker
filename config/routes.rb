a1aade367391d8a6a22a98d97d7402a144c48e3e
Klaus Meyer <spam@klaus-meyer.net>
2026-05-27 21:27:46 +0200

    Use rails default health check mechanism

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   root to: "dashboard#index"
 
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   get "/projects", to: "projects#index", as: :projects
   get "/projects/:source/:slug", to: "projects#show", constraints: { source: /[^\/]+/, slug: /[^\/]+\/[^\/]+/ }, as: :project
 
