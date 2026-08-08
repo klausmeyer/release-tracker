e146193189f2e417da3cba287f2f915c9148c5ce
Klaus Meyer <spam@klaus-meyer.net>
2026-08-08 20:31:41 +0200

    Update solid_queue_monitor to 2.2.1 (#344)

--- config/initializers/solid_queue_monitor.rb
+++ config/initializers/solid_queue_monitor.rb
@@ -2,4 +2,5 @@ SolidQueueMonitor.setup do |config|
   config.authentication_enabled = true
   config.username = ENV["SOLID_QUEUE_MONITOR_USERNAME"]
   config.password = ENV["SOLID_QUEUE_MONITOR_PASSWORD"]
+  config.csrf_protection_enabled = true
 end
